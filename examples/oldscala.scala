efbccc2ef0e9328be5aef085b20acfa31182b488
Yun Mao <yunmao@gmail.com>
2011-02-24 14:30:29 -0500

    simplified

--- examples/oldscala.scala
+++ examples/oldscala.scala
@@ -1,7 +1,5 @@
 #!/bin/sh
 exec scala -savecompiled $0 $@
 !#
-println("Hello, world, from a script! " + (args toList) )
-//throw new RuntimeException("hehe")
-
+println("Hello world, from a script! " + (args toList) )
 
